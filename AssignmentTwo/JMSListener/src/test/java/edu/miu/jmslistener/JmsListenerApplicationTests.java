66b7f59c667644a1a6fa2220d533feca768d1998
Abraham Bisrat <abrahammehari0@gmail.com>
2022-07-03 14:58:38 -0500

    Added jms client

---
+++ AssignmentTwo/JMSListener/src/test/java/edu/miu/jmslistener/JmsListenerApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.jmslistener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmsListenerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
